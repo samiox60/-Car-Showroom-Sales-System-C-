f66442c05225aa58dcc04592a5af709eb9680fd0
W-N-R <waleednaeem133@gmail.com>
2025-03-24 23:32:33 +0500

    .

--- Showroom.h
+++ Showroom.h
@@ -3,8 +3,34 @@
 #define SHOWROOM_H
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 class ShowRoom {
-	
+	string Name_of_showroom;
+	string Address;
+	int contact_number;
+	string email;
+	int sales_tax_registration_number;
+public:
+	//ShowRoom();
+	ShowRoom(string, string, int, string, int);
+	void display() {
+		cout << "*******************************************************";
+		cout << "Name of Showroom: "<<"*****************" << Name_of_showroom << endl;
+		cout << "Address: " << "*****************" << Address << endl;
+		cout << "Contact Number: " << "*****************" << contact_number << endl;
+		cout << "Email: " << "*****************" << email << endl;
+		cout << "Sales Tax Registration Number: " << "*****************" << sales_tax_registration_number << endl;
+		cout << "*******************************************************";
+	}
 	};
+ShowRoom::ShowRoom(string name, string address, int contact, string email, int sale_tax) {
+	Name_of_showroom = name;
+	Address = address;
+	contact_number = contact;
+	email = email;
+	sales_tax_registration_number = sale_tax;
+}
+
 #endif 
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "ShowRoom.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	ShowRoom showroom("Car Showroom", "123, ABC Street, XYZ City", 1234567890, "waleednaeem133@gmail.com", 9119);
+	showroom.display();
+
+	return 0;
+
+}
